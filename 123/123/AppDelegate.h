561ad431fb8bcac05c68b9ad4e9b8d6ce52c567a
a2657222 <1713182456@qq,com>
2012-09-14 14:07:56 +0800

    prefect

---
+++ 123/123/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  123
+//
+//  Created by yrf on 12-9-14.
+//  Copyright (c) 2012年 yrf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 123/123/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  123
+//
+//  Created by yrf on 12-9-14.
+//  Copyright (c) 2012年 yrf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
